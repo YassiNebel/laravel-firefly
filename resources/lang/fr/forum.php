d53ad5bfe87d273e88dece225ac985dca635c4e9
YassiNebel <yasssinebel@gmail.com>
2017-11-08 14:18:05 +0000

    Update forum.php

--- resources/lang/fr/forum.php
+++ resources/lang/fr/forum.php
@@ -7,15 +7,15 @@ return [
     'sr_nav_label' => 'Basculer la navigation',
 
     // Labels
-    'title'        => 'Titre',
-    'content'      => 'Contenu',
-    'name'         => 'Nom',
-    'color'        => 'Couleur',
+    'title'   => 'Titre',
+    'content' => 'Contenu',
+    'name'    => 'Nom',
+    'color'   => 'Couleur',
 
     // Buttons
-    'submit'       => 'Envoyer',
-    'save'         => 'Sauvegarder',
-    'delete'       => 'Supprimer',
-    'edit'         => '&Eacute;diter',
+    'submit' => 'Envoyer',
+    'save'   => 'Sauvegarder',
+    'delete' => 'Supprimer',
+    'edit'   => '&Eacute;diter',
 
 ];
